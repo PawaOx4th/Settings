2cc8ee2d307dbd0fd0889d79f5a0e8fb30397c32
pawaox4th <pawaox4th@gmail.com>
2021-11-15 15:37:39 +0700

    docs : ✏️ Create config emoji for `git-cz`

---
+++ git-cz/emojiCustom.js
@@ -0,0 +1,55 @@
+/**
+ * Provide : Emoji custom for replace on CLI.js file
+ */
+const types = {
+  chore: {
+    description: "Build process or auxiliary tool changes",
+    emoji: "🚨",
+    value: "chore"
+  },
+  ci: {
+    description: "CI related changes",
+    emoji: "🎡",
+    value: "ci"
+  },
+  docs: {
+    description: "Documentation only changes",
+    emoji: "✏️",
+    value: "docs"
+  },
+  feat: {
+    description: "A new feature",
+    emoji: "🍻",
+    value: "feat"
+  },
+  fix: {
+    description: "A bug fix",
+    emoji: "🚧",
+    value: "fix"
+  },
+  perf: {
+    description: "A code change that improves performance",
+    emoji: "⚡️",
+    value: "perf"
+  },
+  refactor: {
+    description: "A code change that neither fixes a bug or adds a feature",
+    emoji: "💡",
+    value: "refactor"
+  },
+  release: {
+    description: "Create a release commit",
+    emoji: "🏹",
+    value: "release"
+  },
+  style: {
+    description: "Markup, white-space, formatting, missing semi-colons...",
+    emoji: "💄",
+    value: "style"
+  },
+  test: {
+    description: "Adding missing tests",
+    emoji: "🧪",
+    value: "test"
+  }
+}
